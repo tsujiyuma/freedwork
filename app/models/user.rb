3e7fea6eaf97f8f33b88aad55d1434db9e0ca321
tsujiyuma <ktw.831.y@gmail.com>
2024-07-11 19:52:40 +0900

    form_withへの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+  validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'は半角英字と半角数字それぞれを含めて設定してください'}
+
 end
