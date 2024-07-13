7dc3617d113fd9499c66ed4619128987a1763690
tsujiyuma <ktw.831.y@gmail.com>
2024-07-13 18:36:09 +0900

    コメント機能の実装
    コメント投稿機能
    タスク詳細ページへの表示

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,26 @@
 class CommentsController < ApplicationController
+
+  def new
+    @project = Project.find(params[:project_id])
+    @comment = Comment.new
+  end
+
+  def create
+    @project = Project.find(params[:project_id])
+    @task = Task.find(params[:task_id])
+    @comment = @task.comments.new(com_params)
+    if @comment.save
+      redirect_to project_task_path(@project, @task)
+    else
+      puts @comment.errors.full_messages
+      render 'new'
+    end
+  end
+
+private
+
+  def com_params
+    params.require(:comment).permit(:comment).merge(user_id: current_user.id)
+  end
+
 end
